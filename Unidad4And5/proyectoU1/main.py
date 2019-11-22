2266ba5a9a8f7753d7ba94f871dad021a0f7a4b0
GeovaniTuz <geovani.tuzpoot@itsva.edu.mx>
2019-11-22 11:00:31 -0600

    importacion de libreria y lista de tokens

--- Unidad4And5/proyectoU1/main.py
+++ Unidad4And5/proyectoU1/main.py
@@ -1,1 +1,15 @@
-print ("hello word")
+#-*- coding: utf-8 -*-
+# desarollo de aanalizardor lexico
+
+#inportacion de librerias necesarias
+import ply.lex as lex
+import re
+import codecs
+import os
+import sys
+
+# lista de tockes necesarios para el compilador
+
+tokens = ['',
+
+]
