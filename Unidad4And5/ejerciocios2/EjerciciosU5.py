17000214aa83cf22b110a392ff6d22a0262378e5
GeovaniTuz <geovani.tuzpoot@itsva.edu.mx>
2019-11-25 23:55:35 -0600

    Unificacion del los ejercicios individuales

--- Unidad4And5/ejerciocios2/EjerciciosU5.py
+++ Unidad4And5/ejerciocios2/EjerciciosU5.py
@@ -44,6 +44,22 @@ print("En las operaciones booleanas son:", resultadoBOOLEANO)
 print ("")
 
 # Formulas más complejas del tipo c = a op ( b op d)
-
+#([A-Za-z0-9-_]=+\s*[A-Za-z0-9-_]+\s*[<,>,!,<=,>=,*,+,/,-]=+\s*([A-Za-z0-9-_]+\s*[<,>,!,<=,>=,*,+,/,-]+\s*[A-Za-z0-9-_]+\s*)+)
+patronCOMPLEJO = r'([A-Za-z0-9]+\s*=+\s*[A-Za-z0-9]+\s*[|<|>|!|<=|>=|\+|\-\*\/]\s*[(]+\s*[A-Za-z0-9]+\s*[|<|>|!|<=|>=|\+|\-\*\/]+\s*[A-Za-z0-9]+\s*[)])'
+resultadoCOMPLEJO = re.findall(patronCOMPLEJO,texto)
+print("\n ENTRADA DE CUALQUIER OPERADOR, formula compleja:\n", resultadoCOMPLEJO)
+print("")
 
 # El encabezado de estructura de control. if, while, for, etc.
+patronFOR = r'(for+\s*[A-Za-z0-9-_]+\s*[in]+\s*[A-Za-z0-9-_]+\s*:)'
+patronWHILE = r'(while+\s*[A-Za-z0-9-_]+\s*[|<|>|!|<=|>=]=+\s*[A-Za-z0-9-_]+\s*:)'
+patronIF = r'(if+\s*[A-Za-z0-9-_]+\s*[|<|>|!|<=|>=]=+\s*[A-Za-z0-9-_]+\s*:)'
+patronTRY = r'(try:+\s*[A-Za-z0-9-_]+\s*except+\s*[A-Za-z0-9-_]+\s*:)'
+resultadoFOR = re.findall(patronFOR, texto)
+resultadoWHILE = re.findall(patronWHILE,texto)
+resultadoIF = re.findall(patronIF,texto)
+resultadoTRY = re.findall(patronTRY,texto)
+print("\n Las operaciones de encabezado FOR son:\n", resultadoFOR)
+print("\n Las operaciones de encabezado WHILE son:\n", resultadoWHILE)
+print("\n Las operaciones de encabezado IF son:\n", resultadoIF)
+print("\n Las operaciones de encabezado TRY son:\n", resultadoTRY)
