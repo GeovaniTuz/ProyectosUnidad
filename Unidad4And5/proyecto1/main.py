fbb81c1ec17f08e620d7da2107d5f2e28e610b42
GeovaniTuz <geovani.tuzpoot@itsva.edu.mx>
2019-11-25 23:47:17 -0600

    creacion de librerias del proyecto integrador

--- Unidad4And5/proyecto1/main.py
+++ Unidad4And5/proyecto1/main.py
@@ -1,7 +1,7 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # desarollo de aanalizardor lexico
 
-#inportacion de librerias necesarias
+# inportacion de librerias necesarias
 #import ply.lex as lex
 import re
 import codecs
@@ -12,13 +12,28 @@ import sys
 
 tokens = ['',
 
-]
+          ]
 
-#lista de palabras reservadas
+# lista de palabras reservadas
 
 reservadas = {
-
-
+    '__halt_compiler',
+    'abstract', 'and', 'array', 'as', 'break',
+    'callable', 'case', 'catch', 'class',
+    'clone', 'const', 'continue', 'declare',
+    'default', 'die', 'do', 'echo',
+    'else', 'elseif',
+    'empty', 'enddeclare', 'endfor',
+    'endforeach', 'endif', 'endswitch',
+    'endwhile', 'eval', 'exit', 'extends',
+    'final', 'for', 'foreach', 'function',
+    'global', 'goto', 'if', 'implements',
+    'include', 'include_once', 'instanceof',
+    'insteadof', 'interface', 'isset', 'list',
+    'namespace', 'new', 'or', 'print', 'private',
+    'protected', 'public', 'require', 'require_once',
+    'return', 'static', 'switch', 'throw', 'trait',
+    'try', 'unset', 'use', 'var', 'while', 'xor'
 }
 
-print ("termino de ejecucion")
+print("termino de ejecucion")
