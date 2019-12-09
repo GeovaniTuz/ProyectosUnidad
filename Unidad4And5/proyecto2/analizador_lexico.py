2367d6e125f0855ee6769f7206242816b6a2f3ae
GeovaniTuz <geovani.tuzpoot@itsva.edu.mx>
2019-12-09 10:45:47 -0600

    En proceso solo falta detalle

--- Unidad4And5/proyecto2/analizador_lexico.py
+++ Unidad4And5/proyecto2/analizador_lexico.py
@@ -1,268 +1,245 @@
-import ply.lex as lex
+# -*- coding: utf-8 -*-
 
-# resultado del analisis
+import ply.lex as lex  # inportacion de librerias necesarias
+import re
+
+# desarollo de aanalizardor lexico
+# proyecto unidad 4
+# liga https://github.com/GeovaniTuz/PythonU4and5
+
 resultado_lexema = []
 
-reservada = (
-    # Palabras Reservadas
-    'INCLUDE',
-    'USING',
-    'NAMESPACE',
-    'STD',
-    'COUT',
-    'CIN',
-   'GET',
-   'CADENA',
-  'RETURN',
-   'VOID',
-    'INT',
-    'ENDL',
-)
-tokens = reservada + (
-    'IDENTIFICADOR',
-    'ENTERO',
-    'ASIGNAR',
-    'TAGINICIO', 'TAG_FINAL', 'ARIBASIS', 'ARIAVANZ',
 
+tokens = [
+    # inicio y final
+    'TAGINICIO', 'TAG_FINAL',
+    'ENTERO', 'ASIGNAR',
     'SUMA',
     'RESTA',
     'MULT',
     'DIV',
     'POTENCIA',
     'MODULO',
-
-   'MINUSMINUS',
-   'PLUSPLUS',
-   'DECIMAL',
-
-    #Condiones
-   'SI',
-    'SINO',
-    #Ciclos
-   'MIENTRAS',
-   'PARA',
-    #logica
-    'AND',
-    'OR',
-    'NOT',
-    'MENORQUE',
-    'MENORIGUAL',
-    'MAYORQUE',
-    'MAYORIGUAL',
-    'IGUAL',
-    'DISTINTO',
+    'MINUSMINUS',
+    'PLUSPLUS',
+    'PUNTOYCOMA',
+    'PUNTO',
+    'COMA', 'DECIMAL', 'VARIABLE', 'COMENTARIO',
+    # Condiones
+    'SI', 'SINO',
+    # Ciclos
+    'MIENTRAS', 'PARA', 'ECHO',
+    # logica
+    'AND', 'OR', 'NOT', 'MENORQUE', 'MENORIGUAL', 'MAYORQUE', 'MAYORIGUAL', 'IGUAL', 'DISTINTO',
     # Symbolos
-    'NUMERAL',
-
-    'PARIZQ',
-    'PARDER',
-    'CORIZQ',
-    'CORDER',
-    'LLAIZQ',
-    'LLADER',
-    
-    # Otros
-    'PUNTOCOMA',
-    'COMA',
-    'COMDOB',
-    'MAYORDER', #>>
-    'MAYORIZQ', #<<
-)
+    'NUMERAL', 'PARIZQ', 'PARDER', 'CORIZQ', 'CORDER', 'LLAIZQ', 'LLADER'
+]
 
 # Reglas de Expresiones Regualres para token de Contexto simple
 
+#t_PUNTO = r'[+,-]?[[0-9]*[.]]?[0-9]+'
+
+# [+,-]?[[0-9]*[.]]?[0-9]+t__COMA = r'\,'
+t_PUNTOYCOMA = r';'
 t_SUMA = r'\+'
 t_RESTA = r'-'
 t_MINUSMINUS = r'\-\-'
-# t_PUNTO = r'\.'
+#t_PUNTO = r'\.'
 t_MULT = r'\*'
 t_DIV = r'/'
 t_MODULO = r'\%'
 t_POTENCIA = r'(\*{2} | \^)'
-
 t_ASIGNAR = r'='
-# Expresiones Logicas
+# Expresiones
 t_AND = r'\&\&'
 t_OR = r'\|{2}'
 t_NOT = r'\!'
 t_MENORQUE = r'<'
 t_MAYORQUE = r'>'
-t_PUNTOCOMA = ';'
-t_COMA = r','
 t_PARIZQ = r'\('
 t_PARDER = r'\)'
 t_CORIZQ = r'\['
 t_CORDER = r'\]'
 t_LLAIZQ = r'{'
 t_LLADER = r'}'
-t_COMDOB = r'\"'
 
-#'ARIBASIS', 'ARIAVANZ',
+# --------------------------------------------------
+# desarrollo de tag_inicio y final
+# no solucionado
 
-#def t_ARIBASIS(t):
- #   r'\$[a-zA-Z]+\s*\=\s*\w+.?\d*\s*[*|+|/|-]\s*\w+.?\d*'
-    #return t
+# def t_TAGINICIO(t):
+#  r'(<+[php]>)'
+# return t
 
-#def t_ARIAVANZ(t):
-   # r'\$\w+\s*\=\s*\w+.?\d*\s*[*|+|/|-]\s*[(]{1}\w+.?\d*\s*[*|+|/|-]\s*\w+.?\d*[)]'
-   # return t
+# tag final
+# --------------------------------------------------
+
+# palabras reservadas de PHP
+
 
 def t_TAGINICIO(t):
     r'(<+[\?+php]+)'
     return t
 
+
 def t_TAG_FINAL(t):
     r'([\?>]+)'
     return t
 
-def t_INCLUDE(t):
-    r'include'
+
+def t_DECIMAL(t):
+    r'([0-9][.]]?[0-9]+)'
     return t
 
 
-
-def t_USING(t):
-    r'using'
+def t_VARIABLE(t):
+    r'([\$]+[A-Za-z]+)'
     return t
 
-def t_NAMESPACE(t):
-    r'namespace'
-    return t
-
-def t_STD(t):
-    r'std'
-    return t
-
-def t_COUT(t):
-    r'cout'
-    return t
-
-def t_CIN(t):
-    r'cin'
-    return t
-
-def t_GET(t):
-    r'get'
-    return t
-
-def t_ENDL(t):
-    r'endl'
-    return t
 
 def t_SINO(t):
     r'else'
     return t
 
+
 def t_SI(t):
     r'if'
     return t
 
-def t_RETURN(t):
-   r'return'
-   return t
 
-def t_VOID(t):
-   r'void'
-   return t
+def t_RETURN(t):
+    r'return'
+    return t
+
+
+# def t_VOID(t):
+    # r'void'
+    # return t
+
+
+def t_ECHO(t):
+    r'echo'
+    return t
+
 
 def t_MIENTRAS(t):
     r'while'
     return t
 
+
 def t_PARA(t):
     r'for'
     return t
+
 
 def t_ENTERO(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-def t_DECIMAL(t):
-    r'\d+\.\d+'
-    t.value = float(t.value)
-    return t
-
-
-def t_IDENTIFICADOR(t):
-    r'\w+(_\d\w)*'
-    return t
-
-def t_CADENA(t):
-   r'\"?(\w+ \ *\w*\d* \ *)\"?'
-   return t
 
 def t_NUMERAL(t):
     r'\#'
     return t
 
+# operacion logica
+
+
 def t_PLUSPLUS(t):
     r'\+\+'
     return t
+
 
 def t_MENORIGUAL(t):
     r'<='
     return t
 
+
 def t_MAYORIGUAL(t):
     r'>='
     return t
+
 
 def t_IGUAL(t):
     r'=='
     return t
 
+
 def t_MAYORDER(t):
     r'<<'
     return t
+
 
 def t_MAYORIZQ(t):
     r'>>'
     return t
 
+
 def t_DISTINTO(t):
     r'!='
     return t
+
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-def t_comments(t):
+
+def t_COMENTARIO(t):
     r'/\*(.|\n)*?\*/'
     t.lexer.lineno += t.value.count('\n')
     print("Comentario de multiple linea")
 
-def t_comments_ONELine(t):
-     r'\/\/(.)*\n'
-     t.lexer.lineno += 1
-     print("Comentario de una linea")
-t_ignore =' \t'
 
-def t_error( t):
+def t_comments_ONELine(t):
+    r'\/\/(.)*\n'
+    t.lexer.lineno += 1
+    print("Comentario de una linea")
+
+
+t_ignore = ' \t'
+
+
+def t_error(t):
     global resultado_lexema
-    estado = "** Token no valido en la Linea {:4} Valor {:16} Posicion {:4}".format(str(t.lineno), str(t.value),
-                                                                      str(t.lexpos))
+    estado = "** Token no valido en la Linea {:4}".format(str(t.lineno)
+                                                          )
     resultado_lexema.append(estado)
     t.lexer.skip(1)
 
+
 # Prueba de ingreso
+
 def prueba(data):
     global resultado_lexema
 
     analizador = lex.lex()
     analizador.input(data)
-
-    resultado_lexema.clear()
     while True:
         tok = analizador.token()
         if not tok:
             break
         # print("lexema de "+tok.type+" valor "+tok.value+" linea "tok.lineno)
-        estado = "Linea {:4} Tipo {:16} Valor {:16} Posicion {:4}".format(str(tok.lineno),str(tok.type) ,str(tok.value), str(tok.lexpos) )
+        estado = "Linea {:4} Tipo {:4} Valor {:4}".format(
+            str(tok.lineno), str(tok.type), str(tok.value))
         resultado_lexema.append(estado)
+
     return resultado_lexema
 
- # instanciamos el analizador lexico
-analizador = lex.lex()
 
+# abrir archivo
+analizador = lex.lex()
+path = "index.php"
+
+try:
+    archivo = open(path, 'r')
+except:
+    print("el archivo no se encontro")
+    quit()
+
+text = ""
+for linea in archivo:
+    text += linea
+prueba(text)
+# AL IMPRIMIR LOS DATOS, ESTO LO ORDENA DE MANERA ESTRUCTURADA
+print('\n'.join(list(map(''.join, resultado_lexema))))
--- Unidad4And5/proyecto2/main.py
+++ Unidad4And5/proyecto2/main.py
@@ -1,32 +1,52 @@
 # -*- coding: utf-8 -*-
 import ply.yacc as yacc
 from analizador_lexico import tokens
-#from analizador_lexico import analizador
+from analizador_lexico import analizador
 import ply.lex as lex  # inportacion de librerias necesarias
 import re
 
 # resultado del analisis
 resultado_gramatica = []
 
+# se declara que se debe de hacer
 precedence = (
     ('right', 'ASIGNAR'),
     ('left', 'SUMA', 'RESTA'),
     ('left', 'MULT', 'DIV'),
-    ('right', 'UMINUS',),
+    ('right', 'UMINUS'),
 )
 nombres = {}
 
+
+# def p_declaracion_decimall(p):
+#    'declaracion : DECIMAL'
+#    print("decimal")
+
+# se definde como debe de funcionar
 def p_declaracion_asignar(t):
-    'declaracion : IDENTIFICADOR ASIGNAR expresion PUNTOCOMA'
+    'declaracion : VARIABLE ASIGNAR expresion PUNTOYCOMA '
     nombres[t[1]] = t[3]
 
-def p_declaraxion_taginicio(p):
-    'declaracion : TAGINICIO'
-    print("Inicio de sintaxis PHP")
+# def p_declaracion_asignar(t):
+ #   'declaracion : IDENTIFICADOR ASIGNAR expresion PUNTOCOMA'
+  #  nombres[t[1]] = t[3]
 
-def p_declaraxion_tag_final(p):
-    'declaracion : TAG_FINAL'
-    print("Final de sintaxis")
+
+# aqui la idea de como hacer la primera declaracion
+def p_declaracion_taginicio(t):
+    'declaracion : TAGINICIO ASIGNAR expresion TAG_FINAL'
+    inicio[t[1]] = t[3]
+ #   print("no cumplio")
+
+# def p_declaracion_taginicio(p):
+ #   'declaracion : TAGINICIO'
+  #  print("Inicio de sintaxis PHP")
+
+
+# def p_declaraxion_tag_final(p):
+ #   'declaracion : TAG_FINAL'
+  #  print("Final de sintaxis")
+
 
 def p_declaracion_expr(t):
     'declaracion : expresion'
@@ -75,110 +95,47 @@ def p_expresion_grupo(t):
     '''
     t[0] = t[2]
 # sintactico de expresiones logicas
-# sintactico de expresiones logicas
 
-
-def p_expresion_logicas(t):
-    '''
-    expresion   :  expresion MENORQUE expresion 
-                |  expresion MAYORQUE expresion 
-                |  expresion MENORIGUAL expresion 
-                |   expresion MAYORIGUAL expresion 
-                |   expresion IGUAL expresion 
-                |   expresion DISTINTO expresion
-                |  PARIZQ expresion PARDER MENORQUE PARIZQ expresion PARDER
-                |  PARIZQ expresion PARDER MAYORQUE PARIZQ expresion PARDER
-                |  PARIZQ expresion PARDER MENORIGUAL PARIZQ expresion PARDER 
-                |  PARIZQ  expresion PARDER MAYORIGUAL PARIZQ expresion PARDER
-                |  PARIZQ  expresion PARDER IGUAL PARIZQ expresion PARDER
-                |  PARIZQ  expresion PARDER DISTINTO PARIZQ expresion PARDER
-    '''
-    if t[2] == "<":
-        t[0] = t[1] < t[3]
-    elif t[2] == ">":
-        t[0] = t[1] > t[3]
-    elif t[2] == "<=":
-        t[0] = t[1] <= t[3]
-    elif t[2] == ">=":
-        t[0] = t[1] >= t[3]
-    elif t[2] == "==":
-        t[0] = t[1] is t[3]
-    elif t[2] == "!=":
-        t[0] = t[1] != t[3]
-    elif t[3] == "<":
-        t[0] = t[2] < t[4]
-    elif t[2] == ">":
-        t[0] = t[2] > t[4]
-    elif t[3] == "<=":
-        t[0] = t[2] <= t[4]
-    elif t[3] == ">=":
-        t[0] = t[2] >= t[4]
-    elif t[3] == "==":
-        t[0] = t[2] is t[4]
-    elif t[3] == "!=":
-        t[0] = t[2] != t[4]
-
-    # print('logica ',[x for x in t])
-
-# gramatica de expresiones booleanadas
-
-
-def p_expresion_booleana(t):
-    '''
-    expresion   :   expresion AND expresion 
-                |   expresion OR expresion 
-                |   expresion NOT expresion 
-                |  PARIZQ expresion AND expresion PARDER
-                |  PARIZQ expresion OR expresion PARDER
-                |  PARIZQ expresion NOT expresion PARDER
-    '''
-    if t[2] == "&&":
-        t[0] = t[1] and t[3]
-    elif t[2] == "||":
-        t[0] = t[1] or t[3]
-    elif t[2] == "!":
-        t[0] = t[1] is not t[3]
-    elif t[3] == "&&":
-        t[0] = t[2] and t[4]
-    elif t[3] == "||":
-        t[0] = t[2] or t[4]
-    elif t[3] == "!":
-        t[0] = t[2] is not t[4]
 
 def p_expresion_numero(t):
     'expresion : ENTERO'
     t[0] = t[1]
 
-def p_expresion_decimal(t):
-    'expresion : DECIMAL'
-    t[0] = t[1]
 
-def p_expresion_cadena(t):
-    'expresion : COMDOB expresion COMDOB'
-    t[0] = t[2]
+# def p_expresion_cadena(t):
+   # 'expresion : COMDOB expresion COMDOB'
+   # t[0] = t[2]
+
 
 def p_expresion_nombre(t):
-    'expresion : IDENTIFICADOR'
+    'expresion : VARIABLE'
     try:
         t[0] = nombres[t[1]]
     except LookupError:
-        print("Nombre desconocido ", t[1])
+        print("---------------------------------------------")
+        print("SINTAXIS ERROR ")
+        print("--------------------------------------------")
+        print("")
+        print("VERIFICAR: ", t[1])
+        print("¿verificar si se definio o asigno un valor(en caso de operacion arimetica)?")
         t[0] = 0
+        print("---------------------------------------------")
 
 
 def p_error(t):
     global resultado_gramatica
     if t:
-        #type: es para el tipo
-        #value:valor que hace que falle
-        #lineno: es para que linea pertenece
-        #lexpos: para la posicion donde se encuentra
-        resultado = "Error sintactico de tipo {} en el valor {} de linea {}".format(
-            str(t.type), str(t.value), str(t.lineno))
-        print(resultado)
+        # type: es para el tipo
+        # value:valor que hace que falle
+        # lineno: es para que linea pertenece
+        # lexpos: para la posicion donde se encuentra
+        resultado = "Error sintactico de tipo {} en el valor {}".format(
+            str(t.type), str(t.value))
+       # print("SINTAXIS ERROR")
+       # print(resultado)
     else:
         resultado = "Error sintactico {}".format(t)
-        print(resultado)
+       # print(resultado)
     resultado_gramatica.append(resultado)
 
 
@@ -190,10 +147,6 @@ def prueba_sintactica(data):
     global resultado_gramatica
    # resultado_gramatica.clear()
 
-    print('-------------------------------------------------')
-    print('-----Resultado del patron encontrado-------------')
-    print('-------------------------------------------------')
-    print('')
     for item in data.splitlines():
         if item:
             gram = parser.parse(item)
@@ -204,6 +157,8 @@ def prueba_sintactica(data):
 
     #print("result: ", resultado_gramatica)
     return resultado_gramatica
+
+
 path = "index.php"
 
 try:
@@ -214,64 +169,42 @@ except:
 
 text = ""
 for linea in archivo:
-   text += linea
+    text += linea
 
 prueba_sintactica(text)
 
-## agregamos un pratron
-#ESTA TOMANDO LOS VALORES QUE ENCUENTRE COMO UNA EXPRESION BASICA DE DOS NUMEROS REALIZANDO UNA OPERACION
-#([\$?][a-z0-9|0-9.0-9]+\s*[\+|\-|\*|\/]+\s*[\$]?[a-z0-9-_|0-9.0-9]+)
-#LOS DOS FUNCIONAN PERO DETALLES ES --> SEPARACION DE LETRAS Y NUMEROS
-patronbasico = r'[\$?][a-z|0-9.0-9]+\s*[\+|\-|\*|\/]+\s*[\$]?[a-z|0-9.0-9]+'
-patronBLetra = r'[\$?][a-z]+\s*[\+|\-|\*|\/]+\s*[\$]?[a-z]+'
-patronBNum = r'[0-9.0-9]+\s*[\+|\-|\*|\/]+\s*[0-9.0-9]+'
-#funciona pero tiene detalles --> SEPARACION DE NUMEROS CON LETRAS
-patronavanzado = r'[\$?][a-z|0-9.0-9\s*\+|\-|\*|\/\s*\$?a-z|0-9.0-9]+\s*[(?][\$?a-z|0-9.0-9\s*\+|\-|\*|\/\s*\$?a-z|0-9.0-9]+[)?]'
-patronALetra = r'[\$?][a-z\s*\+|\-|\*|\/\s*\$?a-z|]+\s*[(?][\$?a-z\s*\+|\-|\*|\/\s*\$?a-z]+[)?]'
-patronANum = r'[0-9.0-9\s*\+|\-|\*|\/\s*0-9.0-9]+\s*[(?][0-9.0-9\s*\+|\-|\*|\/\s*0-9.0-9]+[)?]'
-patrooo  = r'\d+\.\d+'
-#se analiza con el patron
-resultadobasico = re.findall(patronbasico, text)
-resultadoBLetra = re.findall(patronBLetra, text)
-resultadoBNum = re.findall(patronBNum, text)
-resultadoavanzado = re.findall(patronavanzado,text)
-resultadoALetra = re.findall(patronALetra, text)
-resultadoANum = re.findall(patronANum, text)
-resultado = re.findall(patrooo, text)
+# agregamos un pratron
+patronsin = r'[a-zA-Z]'
+# se analiza con el patron
+resultadoCOMPLEJO = re.findall(patronsin, text)
 
 
 #print ("avanzado", text)
 #print(''.join(list(map(''.join, text))))
 
-# se imprime el patron 
-print('-------------------------------------------------')
-print('------------Patron encontrado--------------------')
-print('-------------------------------------------------')
-print('')
-print("Avanzado", resultadoavanzado, resultadoALetra, resultadoANum)
-print('')
-print("Basico", resultadobasico, resultadoBLetra, resultadoBNum)
-print('')
-print(resultado)
+# se imprime el patron
+# print('-------------------------------------------------')
+#print('------------Patron encontrado--------------------')
+# print('-------------------------------------------------')
+# print('')
+#print("Avanzado", resultadoCOMPLEJO)
+# print('')
+#print("Basico", resultadoCOMPLEJO)
+# print('')
 
 # aqui se imprime los resultados de la operacion
-print('-------------------------------------------------')
-print('-----contenido del archivo index.php-------------')
-print('-------------------------------------------------')
-print('')
-print(''.join(list(map(''.join, text))))
-print('')
+# print('-------------------------------------------------')
+#print('-----contenido del archivo index.php-------------')
+# print('-------------------------------------------------')
+# print('')
+#print(''.join(list(map(''.join, text))))
+# print('')
 
-print('-------------------------------------------------')
-print('--------resultados de la operaciones-------------')
-print('-------------------------------------------------')
+#print('-------------------------------------------------')
+#print('----------resultado de la gramatica-------------')
+#print('-------------------------------------------------')
+
+#print("es correcto")
+
 print('\n'.join(list(map(''.join, resultado_gramatica))))
-
-
-#1.-agregar patrones para saber si es una operacion basica o avanzada
-
-#2.-falta: la estrutura de control {if}{else}
-
-#3.-solo falta resta
-
-#solo faltan estos tres puntos, agaren uno para terminar
+#print("SINTAXIS CORRECTA")
