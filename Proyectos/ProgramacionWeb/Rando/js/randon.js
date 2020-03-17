68a009d8791838a2976432574f57000998cecc1a
GeovaniTuz <geovani.tuzpoot@itsva.edu.mx>
2020-03-16 20:13:06 -0600

    ya listo de rusell solo falta algunos detalles esteticos

--- Proyectos/ProgramacionWeb/Rando/js/randon.js
+++ Proyectos/ProgramacionWeb/Rando/js/randon.js
@@ -25,40 +25,50 @@ function numerorandom() {
     }
     var pruebapalabra;
     pruebapalabra = textoTroceado.length;
-    
+
     // total de palabras que existen. 
     console.log(pruebapalabra)
     //-------------------------------------
     var contador = [];
-    contador = textoTroceado.length;
-    console.log(contador);
+
+    //console.log(contador);
     document.getElementById('resultadorandom').innerHTML = pruebapalabra;
     //FIN DEL CONTADOR DE PALABRAS
 
 
     var randonum = Math.floor(Math.random() * pruebapalabra);
     console.log(randonum);
-    
+
     document.getElementById('resultadorandom3').innerHTML = randonum;
 
     var iniciolink = "<a href='https://www.w3schools.com/colors/colors_picker.asp'>", finlink = "</a>";
-    var iniciop = "<p>", finp = "</p>", espa = "<br>";
+    var iniciop = "<p>", finp = "</p>", punto = ". ", saltar = "<br>";
+    var elemento = " TEXTO TOMADO DE LA CAJA Y TROCEADO --> ", elemento2 = "Numero random generado: ";
+    //var n = a + "<br>" + b + "<br>" + c + "<br>" + d;
 
     //document.write(espa);
-
-    document.write(iniciop);
+   // document.write(iniciop); // INICIO DE LA ETIQUETA P
     for (var i = 0; i < randonum; i++) {
+        contador = textoTroceado;
         var les = iniciolink + contador[i] + finlink;
-       // document.write(iniciolink + contador[i] + finlink);
-        document.write(les);
+        // document.write(iniciolink + contador[i] + finlink);
+        // les.toString();
+        //var cade = les.toString();
+        document.write(" ", les);
+        //document.write(punto);
 
-       
 
-        document.write(espa);
     }
-    //document.write(finp);
+
     // total de palabras que se deben de hacer viculo
-    console.log(i);
+    //console.log(i);
+
+   // document.write(saltar + saltar + elemento + contador, saltar + saltar + elemento2 + randonum);
+    //var cade = les.toString();
+
+
+   // document.write(finp); //FIN DE LA ETIQUETA P
     //FIN DE GENERACION DE NUMERO RANDOM
 
 }
+
